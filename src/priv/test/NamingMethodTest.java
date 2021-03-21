e3f3de3681db0796ad2e1c78cb19ddf16dd777f0
ihch17 <ihatecurlhair@aliyun.com>
2021-03-21 23:35:06 +0800

    整理包结构

--- src/priv/test/NamingMethodTest.java
+++ src/priv/test/NamingMethodTest.java
@@ -1,4 +1,4 @@
-package priv.priv.test;
+package priv.test;
 
 import priv.pojo.SoilSample;
 import priv.utils.NamingUtil;
@@ -10,10 +10,12 @@ public class NamingMethodTest {
 
     public static void main(String[] args) {
 
+        // 实例化一个土样
         //SoilSample soilSample = new SoilSample();
 
         NamingUtil namingUtil = new NamingUtil();
-
+        //String result = namingUtil.namingMethod();
+        //System.out.println(result);
 
 
     }
